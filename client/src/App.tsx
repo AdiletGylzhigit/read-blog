cba57ff7cefa1e5b35be08252d163f2ec4ed6015
Adilet Gylzhigit <adiletgylzhigit@gmail.com>
2023-06-05 20:25:23 -0800

    Added react-router-dom and Inter font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is home!</div>,
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;

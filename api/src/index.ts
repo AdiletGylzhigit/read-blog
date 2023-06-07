b8b92ecf34f0c046298a1cd98739060765790664
Adilet Gylzhigit <adiletgylzhigit@gmail.com>
2023-06-07 00:12:39 -0800

    Conecting to MongoDb and installing packages for Node.js

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,21 @@
-// Write your logic here
+const express = require("express");
+const { connectToDb, getDb } = require("./db");
+
+// init app & middleware
+const app = express();
+
+// db connection
+let db
+
+connectToDb((err) => {
+  if(!err){
+    app.listen(8080, () => {
+      console.log("Sever connected!");
+    })
+    db = getDb();
+  }
+})
+
+app.use(express.json());
+
+

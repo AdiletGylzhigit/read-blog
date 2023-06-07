324e220dede6bababfaf149c3c799e36f3ec7f86
Adilet Gylzhigit <adiletgylzhigit@gmail.com>
2023-06-07 01:26:02 -0800

    Added cors package

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,10 +3,12 @@ const { connectToDb, getDb } = require("./db");
 const postRoutes = require('./routes/posts');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
+const cors = require('cors');
 
 // init app & middleware
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
